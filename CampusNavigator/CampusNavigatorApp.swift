c8ac0f46d1cc90fed99357282579785ee9d97e7e
Bhanuka <biwanthabhanuka@gmail.com>
2025-02-16 10:09:25 +0530

    Initial Commit

---
+++ CampusNavigator/CampusNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusNavigatorApp.swift
+//  CampusNavigator
+//
+//  Created by Bhanuka  Pannipitiya  on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct CampusNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
